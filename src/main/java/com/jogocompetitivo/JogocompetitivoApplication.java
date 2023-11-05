99f55cb20aa9fc58668bc8e535f5b685682a65cb
Diego Fernandes <diegofrndes@gmail.com>
2023-11-05 15:22:30 -0300

    first commit

---
+++ src/main/java/com/jogocompetitivo/JogocompetitivoApplication.java
@@ -0,0 +1,13 @@
+package com.jogocompetitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogocompetitivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogocompetitivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jogocompetitivo/JogocompetitivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jogocompetitivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogocompetitivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
