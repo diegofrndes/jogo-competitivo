4fa01d9f1c64e1a653c0f56bbade6efcad40cc63
Diego Fernandes <diegofrndes@gmail.com>
2023-11-05 18:11:45 -0300

    feat: user repository

--- src/main/java/com/jogocompetitivo/repositories/UserRepository.java
+++ src/main/java/com/jogocompetitivo/repositories/UserRepository.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.jogocompetitivo.domain.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-	@Query("SELECT u FROM User u ORDER BY u.elo ASC, u.pdl ASC")
+	@Query(value = "SELECT u FROM User u ORDER BY u.elo ASC, u.balance ASC", nativeQuery = true)
 	List<User> findAllOrdered();
 }
